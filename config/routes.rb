7080ad8002301e21c6391c33f13af75378056294
hollyjoc <holly.jo.robertson22@gmail.com>
2018-01-30 20:16:26 -0500

    Added momFacebook files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :contacts
+  resources :posts
+  get '/about' => 'pages#about'
+  root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
