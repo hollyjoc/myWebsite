449dacb5c40c6da93a25c0ca454709e6d5757bdb
hollyjoc <holly.jo.robertson22@gmail.com>
2018-02-03 23:37:51 -0500

    Added logos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,8 @@ class PagesController < ApplicationController
 
 	def about
 	end
+
+	def resume
+	end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :contacts
   resources :posts
   get '/about' => 'pages#about'
+  get '/resume' => 'pages#resume'
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
